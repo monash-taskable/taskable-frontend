d061f9b2e3368e1402919724c4ebf45cda93aa99
frankium <frankium@frankium.com>
2024-07-04 23:09:50 +0800

    add and configured i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     ['@pinia/nuxt', {autoImports: 'defineStore'}],
+    ["@nuxtjs/i18n", {
+      locales: [
+        {
+          code: 'en-au',
+          file: 'en-au.json'
+        }
+      ],
+      lazy: true,
+      langDir: 'lang',
+      defaultLocale: 'en-au'
+    }],
   ],
   imports: {
     dirs: ['stores']
