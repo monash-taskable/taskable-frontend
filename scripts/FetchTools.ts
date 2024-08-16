496ee37394ef13432692cdc805186123f3aa04c3
Frankium <frankium@frankium.com>
2024-08-17 00:53:16 +1000

    updated fetch to have POST and GET sends byte data

--- scripts/FetchTools.ts
+++ scripts/FetchTools.ts
@@ -1,4 +1,6 @@
 /**
+ * Make a GET request to a HTTP server
+ * 
  * @param url - the URL to request 
  * @param decoder - the decoder function from a generated protobuf definition 
  * @returns The decoded request result
@@ -17,8 +19,29 @@
  * </script>
  * ```
  */
-export const fetchAndDecode = async <T>(url: string, decoder: (byte: Uint8Array) => T): Promise<T> => {
-  const fetchRes = await (await fetch(url)).text();
-  const bytes = Uint8Array.from(atob(fetchRes), c => c.charCodeAt(0));
+export const getAndDecode = async <T>(url: string, decoder: (byte: Uint8Array) => T): Promise<T> => {
+  const fetchRes = await (await fetch(url)).blob();
+  const bytes = new Uint8Array(await fetchRes.arrayBuffer());
   return decoder(bytes);
 };
+
+/**
+ * Make a POST request to a HTTP server
+ * 
+ * @param url - the URL to request
+ * @param encoder - the encoder function from a generated protobuf definition
+ * @param data - the payload
+ * @param decoder - the decoder function from a generated protobuf definition 
+ * @returns The decoded request result
+ */
+export const postAndDecode = async <T, R>(
+  url: string, encoder: ((_t: T) => Uint8Array), data: T, decoder: (byte: Uint8Array) => R): Promise<R> => {
+
+  const fetchRes = await (await fetch(url, {
+    method: "POST",
+    body: encoder(data)
+  })).blob();
+
+  const bytes = new Uint8Array(await fetchRes.arrayBuffer());
+  return decoder(bytes);
+}
