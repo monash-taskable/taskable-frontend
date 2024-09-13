1abbfd94066efaa8387c7d6c69af32dd16523a8a
frankium <frankium@frankium.com>
2024-09-13 22:23:17 +1000

    dropdown indev

---
+++ components/dropdown.vue
@@ -0,0 +1,172 @@
+<template>
+  <select :tabindex="tabindex"
+      :class="getClass(enterFlag, focusFlag, props.styles)"
+      :style="getStyle(props.styles)" :id="id"
+      @focusout="onFocusOut" @keyup="onKeyUp" @keydown="()=>{}"
+      @click="_onClick"
+      @change="_onClick"
+      >
+      <slot/>
+  </select>
+</template>
+
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+import { buttonStyle, presets, type ButtonStyle } from '~/types/Button';
+
+const props = defineProps({
+  styles: {type: Object as PropType<ButtonStyle>, required: false},
+  tabindex: {type: Number, required: false},
+  id: {type: String, required:false, default: undefined},
+})
+
+// interaction logic
+const enterFlag = ref(false);
+const focusFlag = ref(false);
+const emitClick = defineEmits(["click", "change"]);
+const onKeyUp = () => { focusFlag.value = true; };
+const onFocusOut = () => {
+  enterFlag.value = false;
+  focusFlag.value = false;
+}
+const _onClick = () => {
+  emitClick("click");
+}
+const _onChange = () => {
+  emitClick("change");
+}
+
+// getters
+const getClass = (
+  enter: boolean, focus: boolean, 
+  styles?: ButtonStyle): string => {
+  const enterCls = enter ? "enter" : "";
+  const focusCls = focus ? "focused" : "";
+
+  styles = <ButtonStyle>((styles === undefined) ? presets.get("background") : styles);
+  styles = buttonStyle(styles);
+  const sizeCls = (styles.size === undefined) ? "medium" : styles.size;
+  const focusableCls = (styles.focusable === undefined || styles.focusable) ? "focusable" : "non-focusable";
+  const bgHoverCls = (styles.backgroundColorHover === undefined) ? "" : "bgHover";
+  const fgCls = (styles.foregroundColor === undefined) ? "" : "fg";
+  const fgHoverCls = (styles.foregroundColorHover === undefined) ? "" : "fgHover";
+  const paddingCls = (styles.padding === undefined) ? "" : "padding";
+
+  return [
+    sizeCls,
+    focusableCls,
+    bgHoverCls,
+    fgCls,
+    fgHoverCls,
+    paddingCls,
+    focusCls,
+    enterCls,
+  ].join(" ");
+}
+
+const getStyle = (styles?: ButtonStyle): string => {
+  styles = <ButtonStyle>((styles === undefined) ? presets.get("background") : styles);
+  styles = buttonStyle(styles);
+
+  let styleStr = "";
+
+  if (styles.backgroundColor !== undefined) {
+    styleStr += `--bg: ${styles.backgroundColor};`;
+  }
+
+  if (styles.backgroundColorHover !== undefined) {
+    styleStr += `--bgHover: ${styles.backgroundColorHover};`;
+  }
+
+  if (styles.foregroundColor !== undefined) {
+    styleStr += `--fg: ${styles.foregroundColor};`;
+  }
+
+  if (styles.foregroundColorHover !== undefined) {
+    styleStr += `--fgHover: ${styles.foregroundColorHover};`;
+  }
+
+  if (styles.padding !== undefined) {
+    styleStr += `--padding: ${styles.padding};`;
+  }
+
+  return styleStr;
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import "/assets/styles/constants/Typography.scss";
+@import "/assets/styles/constants/DefaultStyles.scss";
+@import "/assets/styles/constants/Flex.scss";
+@import "/assets/styles/constants/Sizes.scss";
+
+select {
+  @include typemix-label();
+  @include defmix-box();
+  @include flex-row();
+  @include flex-cross(center);
+  @include flex-main(flex-start);
+
+  .caption {
+    overflow-x: hidden;
+    transition: $def-transition;
+    display: inline-block;
+    white-space: nowrap;
+  }
+  
+  gap: $space-medium;
+  transition: $def-transition;
+  background: var(--bg);
+  color: var(--foreground);
+  
+  &>span:not(.caption) {
+    font-size: $icon-size-medium;
+    width: $icon-size-medium;
+    height: $icon-size-medium;
+  }
+}
+
+// sizes
+select {
+  &.small {
+    padding: $space-small;
+  }
+  
+  &.medium {
+    padding: $space-medium;
+  }
+  
+  &.large {
+    padding: $space-large;
+  }
+}
+
+// style option rules
+select {
+  &.focusable {
+    @include defmix-focusable();
+  }
+
+  &.non-focusable {
+    @include defmix-focusable(2px transparent solid);
+  }
+
+
+  &.bgHover:hover, &.enter {
+    background: var(--bgHover);
+  }
+
+  &.fg {
+    color: var(--fg);
+  }
+
+  &.fgHover:hover, &.fgHover.enter {
+    color: var(--fgHover);
+  }
+
+  &.padding {
+    padding: var(--padding);
+  }
+}
+</style>
--- pages/debug.vue
+++ pages/debug.vue
@@ -1,25 +1,16 @@
 <template>
   <div class="page-root">
-    <IconButton @click="debug" :styles="{colorPreset: 'layer'}" caption="debug"/>
-    <IconButton @click="logout" :styles="{colorPreset: 'layer'}" caption="logout"/>
+    <Dropdown :styles="{colorPreset: 'accent-strong'}">
+      <option value="0">hello</option>
+      <option selected value="1">world</option>
+    </Dropdown>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { FetchRequest } from '~/scripts/FetchTools';
-
-definePageMeta({layout:"empty"});
-
-const dialog = useDialogs();
-
-const debug = async () => {
-  const x = await FetchRequest.api("/auth/test").overrideCsrf("b04f01ab-8748-48ba-abb4-d068df4ff").post().commit();
-  console.log(x._response?.text());
-}
-
-const logout = async () => {
-  useAppStateStore().signOut();
-}
+definePageMeta({
+  layout: "empty",
+})
 </script>
 
 <style lang="scss" scoped>
